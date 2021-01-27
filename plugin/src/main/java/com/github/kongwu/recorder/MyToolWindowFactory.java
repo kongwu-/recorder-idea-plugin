079e12c61390723fcb5063e33aafc674ddf0fa68
jiangxin <jiangxin@95303.com>
2021-01-27 18:05:01 +0800

    下班

--- plugin/src/main/java/com/github/kongwu/recorder/MyToolWindowFactory.java
+++ plugin/src/main/java/com/github/kongwu/recorder/MyToolWindowFactory.java
@@ -7,12 +7,18 @@ import com.intellij.execution.ui.RunContentManager;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.fileEditor.FileEditorManager;
+import com.intellij.openapi.fileEditor.OpenFileDescriptor;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.Conditions;
+import com.intellij.openapi.vfs.VirtualFileSystem;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowFactory;
+import com.intellij.psi.JavaPsiFacade;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiMethod;
+import com.intellij.psi.search.GlobalSearchScope;
+import com.intellij.psi.search.searches.ReferencesSearch;
 import com.intellij.ui.content.Content;
 import com.intellij.ui.content.ContentFactory;
 import org.jetbrains.annotations.NotNull;
@@ -25,14 +31,18 @@ public class MyToolWindowFactory extends AnAction {
     @Override
     public void actionPerformed(@NotNull AnActionEvent e) {
         Project project = e.getProject();
-
-        final ExecutionManagerImpl executionManager = ExecutionManagerImpl.getInstance(project);
-        List<RunContentDescriptor> runningDescriptors = executionManager.getRunningDescriptors(Conditions.alwaysTrue());
-        System.out.println("正在运行的进程有……"+runningDescriptors.size());
-        RunContentDescriptor selectedContent = RunContentManager.getInstance(project).getSelectedContent();
-        KillableColoredProcessHandler processHandler = (KillableColoredProcessHandler)selectedContent.getProcessHandler();
-                    Process process = processHandler.getProcess();
-        System.out.println(process.info());
+        FileEditorManager fileEditorManager = FileEditorManager.getInstance(project);
+        PsiElement sourceElement = JavaPsiFacade.getInstance(project).findClass("java.util.ArrayList", GlobalSearchScope.allScope(project)).getSourceElement();
+        OpenFileDescriptor openFileDescriptor = new OpenFileDescriptor(project, sourceElement.getContainingFile().getVirtualFile());
+        fileEditorManager.openTextEditor(openFileDescriptor,true);
+//
+//        final ExecutionManagerImpl executionManager = ExecutionManagerImpl.getInstance(project);
+//        List<RunContentDescriptor> runningDescriptors = executionManager.getRunningDescriptors(Conditions.alwaysTrue());
+//        System.out.println("正在运行的进程有……"+runningDescriptors.size());
+//        RunContentDescriptor selectedContent = RunContentManager.getInstance(project).getSelectedContent();
+//        KillableColoredProcessHandler processHandler = (KillableColoredProcessHandler)selectedContent.getProcessHandler();
+//                    Process process = processHandler.getProcess();
+//        System.out.println(process.info());
 //        for (RunContentDescriptor runningDescriptor : runningDescriptors) {
 //            KillableColoredProcessHandler processHandler = (KillableColoredProcessHandler) runningDescriptor.getProcessHandler();
 //            Process process = processHandler.getProcess();
@@ -44,6 +54,7 @@ public class MyToolWindowFactory extends AnAction {
     @Override
     public void update(@NotNull AnActionEvent e) {
         PsiElement psiElement = e.getData(CommonDataKeys.PSI_ELEMENT);
+
         System.out.println(psiElement.getClass().getName());
         e.getPresentation().setEnabledAndVisible(psiElement instanceof PsiMethod);
 
